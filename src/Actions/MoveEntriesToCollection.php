cc40cc93d9abda502f1ab744616b1fdde8b567df
jelle <jelle@komma.nl>
2025-04-18 09:12:16 +0200

    Improve to allow db usage and multisite for statamic eloquent

--- src/Actions/MoveEntriesToCollection.php
+++ src/Actions/MoveEntriesToCollection.php
@@ -2,12 +2,10 @@
 
 namespace Codedge\MoveEntries\Actions;
 
-
 use Illuminate\Support\Facades\File;
 use Statamic\Actions\Action;
 use Statamic\Entries\Entry;
-use Statamic\Facades\Collection;
-
+use Statamic\Facades;
 
 class MoveEntriesToCollection extends Action
 {
@@ -18,7 +16,35 @@ class MoveEntriesToCollection extends Action
 
     public function run($items, $values): void
     {
-        $collection = Collection::findByHandle($values['collection']);
+        $collection = Facades\Collection::findByHandle($values['collection']);
+
+        $usesDatabase = (bool) data_get(config('statamic.eloquent-driver'), 'entries.driver');
+
+        $usesDatabase
+            ? $this->processDb($items, $collection)
+            : $this->processFlat($items, $collection);
+    }
+
+    protected function processDb($items, $collection): void
+    {
+        $blueprintHandle = $collection->entryBlueprint()->handle;
+        $isMultisite = config('statamic.system.multisite');
+
+        /** @var \Illuminate\Support\Collection $items */
+        foreach($items as $item) {
+            $item->collection($collection)->blueprint($blueprintHandle)->save();
+
+            if($isMultisite){
+                Facades\Entry::query()
+                    ->where('origin', $item->id())
+                    ->get()
+                    ->each(fn ($entry) => $this->updateEntry($entry, $collection, $blueprintHandle));
+            }
+        }
+    }
+
+    protected function processFlat($items, $collection): void
+    {
         $targetDir = preg_replace('/\.' . preg_quote(pathinfo($collection->path(), PATHINFO_EXTENSION), '/') . '$/', '', $collection->path());
 
         /** @var \Illuminate\Support\Collection $items */
@@ -35,8 +61,8 @@ class MoveEntriesToCollection extends Action
             'collection' => [
                 'type' => 'select',
                 'validate' => 'required',
-                'options' => Collection::all()->flatMap(function (\Statamic\Entries\Collection $item) {
-                    return [ $item->handle() => $item->title() ];
+                'options' => Facades\Collection::all()->flatMap(function (\Statamic\Entries\Collection $item) {
+                    return [$item->handle() => $item->title()];
                 })->sort(),
             ],
         ];
